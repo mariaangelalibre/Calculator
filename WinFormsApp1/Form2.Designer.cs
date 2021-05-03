122cd5db5c656b46f5074c51252009ae65ecc831
Maria Angela Libre <mariaangelaplibre@iskolarngbayan.pup.edu.ph>
2021-05-04 03:29:06 +0800

    nyare

--- WinFormsApp1/Form2.Designer.cs
+++ WinFormsApp1/Form2.Designer.cs
@@ -1,15 +1,15 @@
 ﻿
 namespace WinFormsApp1
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -24,8 +24,8 @@ namespace WinFormsApp1
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
@@ -34,21 +34,21 @@ namespace WinFormsApp1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(366, 79);
+            this.button1.Location = new System.Drawing.Point(313, 80);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(143, 103);
+            this.button1.Size = new System.Drawing.Size(132, 76);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // Form1
+            // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "Form2";
+            this.Text = "Form2";
             this.ResumeLayout(false);
 
         }
@@ -58,4 +58,3 @@ namespace WinFormsApp1
         private System.Windows.Forms.Button button1;
     }
 }
-
